fadca8affd3c78fbfc13288d959b77547df37af5
V. Subramani Raju <vsr625@gmail.com>
2018-02-23 10:37:32 +0530

    cleaned up index.js

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,13 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+// Configuring firebase
+admin.initializeApp(functions.config().firebase)
 const database = admin.database()
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
+// Apis start from here
+
+// Add restaurant
 exports.addrest = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
- database.ref('/').set({
-     'name': 'name'
- })
-});
+
+})
