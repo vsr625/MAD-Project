1d26b12ea6239a64a49e514e2c6c4fa777e2b224
V. Subramani Raju <vsr625@gmail.com>
2018-02-23 10:36:43 +0530

    A demo api

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const database = admin.database()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.addrest = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+ database.ref('/').set({
+     'name': 'name'
+ })
+});
